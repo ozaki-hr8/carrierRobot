bd72a454670f2674c89b710a3442f492191e6901
NUC1 <ozaki.aislab@gmail.com>
2022-07-20 12:33:16 +0900

    code format

--- carrierRobot.py
+++ carrierRobot.py
@@ -1,49 +1,55 @@
 import cv2
 import numpy as np
-import sys, select, os
+import sys
+import select
+import os
 import time
 import serial
 import re
+
+# Uses Dynamixel SDK library
 from dynamixel_sdk import *
-from dynamixel_sdk import Protocol2PacketHandler as P2PH                    # Uses Dynamixel SDK library
+from dynamixel_sdk import Protocol2PacketHandler as P2PH
 from dynamixel_sdk import PortHandler
 from dynamixel_sdk import PacketHandler
 
 # Control table address
-AX_TORQUE_ENABLE       = 24                          # Control table address is different in Dynamixel model
-AX_GOAL_POSITION       = 30
-AX_PRESENT_POSITION    = 36
-AX_MOVING_SPEED        = 32
-AX_MOVING              = 46
+# Control table address is different in Dynamixel model
+AX_TORQUE_ENABLE = 24
+AX_GOAL_POSITION = 30
+AX_PRESENT_POSITION = 36
+AX_MOVING_SPEED = 32
+AX_MOVING = 46
 # Protocol version
-PROTOCOL_VERSION            = 1                             # See which protocol version is used in the Dynamixel
+# See which protocol version is used in the Dynamixel
+PROTOCOL_VERSION = 1
 
 # Default setting
-DXL_ID                      = 16     # 左後
-DXL_ID2                     = 12    # 左前
-DXL_ID3                     = 4    # 右後
-DXL_ID4                     = 17    #右前
-DXL_ID5                     = 9    # アーム
-BAUDRATE                    = 1000000
-DEVICENAME                  = "/dev/tty.usbserial-AL03ERS9"                # Check which port is being used on your controller
-                                                            # ex) Windows: "COM1"   Linux: "/dev/ttyUSB0"
+DXL_ID = 16     # 左後
+DXL_ID2 = 12    # 左前
+DXL_ID3 = 4     # 右後
+DXL_ID4 = 17    # 右前
+DXL_ID5 = 9     # アーム
+BAUDRATE = 1000000
+# Check which port is being used on your controller
+DEVICENAME = "/dev/tty.usbserial-AL03ERS9"
+# ex) Windows: "COM1"   Linux: "/dev/ttyUSB0"
 
-TORQUE_ENABLE               = 1                             # Value for enabling the torque
-TORQUE_DISABLE              = 0                             # Value for disabling the torque
-DXL_MINIMUM_POSITION_VALUE  = 819                       # Dynamixel will rotate between this value
-DXL_MAXIMUM_POSITION_VALUE  = 1023
+TORQUE_ENABLE = 1                             # Value for enabling the torque
+TORQUE_DISABLE = 0                            # Value for disabling the torque
+# Dynamixel will rotate between this value
+DXL_MINIMUM_POSITION_VALUE = 819
+DXL_MAXIMUM_POSITION_VALUE = 1023
 
-                       # and this value (note that the Dynamixel would not move when the position value is out of movable range. Check e-manual about the range of the Dynamixel you use.)
+# and this value (note that the Dynamixel would not move when the position value is out of movable range. Check e-manual about the range of the Dynamixel you use.)
 
-ESC_ASCII_VALUE             = 0x1b
+ESC_ASCII_VALUE = 0x1b
 
-COMM_SUCCESS                = 0                             # Communication Success result value
-COMM_TX_FAIL                = -1001                         # Communication Tx Failed
+COMM_SUCCESS = 0                             # Communication Success result value
+COMM_TX_FAIL = -1001                         # Communication Tx Failed
 
 flag = 1
 flag2 = 0
-
-
 # Initialize PortHandler Structs
 # Set the port path
 # Get methods and members of PortHandlerLinux or PortHandlerWindows
@@ -53,7 +59,7 @@ portHandler = PortHandler(DEVICENAME)
 # Set the protocol version
 # Get methods and members of Protocol1PacketHandler or Protocol2PacketHandler
 packetHandler = PacketHandler(PROTOCOL_VERSION)
-cap = cv2.VideoCapture(2) #cameraの設定
+cap = cv2.VideoCapture(2)
 
 def red_detect(img):
     # HSV色空間に変換
@@ -107,7 +113,6 @@ class DXL():
             print("Press any key to terminate...")
             quit()
 
-
         # Set port baudrate
         if portHandler.setBaudRate(BAUDRATE):
             print("Succeeded to change the baudrate")
@@ -125,8 +130,6 @@ class DXL():
         else:
             print("Dynamixel has been successfully connected")
 
-
-
     def moveDXL(self):
         global flag
         # Read moving state
@@ -135,9 +138,9 @@ class DXL():
         dxl_moving_state, dxl_comm_result, dxl_error = packetHandler.read1ByteTxRx(portHandler, DXL_ID3, AX_MOVING_SPEED)
         dxl_moving_state, dxl_comm_result, dxl_error = packetHandler.read1ByteTxRx(portHandler, DXL_ID4, AX_MOVING_SPEED)
         #dxl_moving_state, dxl_comm_result, dxl_error = packetHandler.read1ByteTxRx(portHandler, DXL_ID5, AX_MOVING)
+
         # Write goal position
         if dxl_moving_state == 0:
-
             if flag == 1:
                 dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, DXL_ID, AX_MOVING_SPEED, 200)
                 dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, DXL_ID2, AX_MOVING_SPEED, 200)
@@ -151,8 +154,6 @@ class DXL():
                 dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, DXL_ID2, AX_MOVING_SPEED, 0)
                 dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, DXL_ID3, AX_MOVING_SPEED, 1024)
                 dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, DXL_ID4, AX_MOVING_SPEED, 1024)
-
-
 
                 #右回り
             elif flag ==2:
@@ -209,34 +210,23 @@ class DXL():
                 dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, DXL_ID5, AX_GOAL_POSITION, DXL_MAXIMUM_POSITION_VALUE)
                 flag2 = 1
                 #time.sleep(3)
-
-
-
-
 ################## Main Loop ################################################
 
 if __name__ == "__main__":
 
     dx = DXL()
 
-
-
     try:
         while True:
             #　カメラの読み込み
-            #ret, frame = cap.read()
-            #img = frame
-
             _, img = cap.read()
 
             size = (img.shape[1]//2, img.shape[0]//2)
             img = cv2.resize(img, size)
 
-            #print("press ESC to quit!")
-            #cv2.namedWindow('Camera', cv2.WINDOW_AUTOSIZE)
             mask = red_detect(img)
 
-                 # マスク画像をブロブ解析（面積最大のブロブ情報を取得）
+            # マスク画像をブロブ解析（面積最大のブロブ情報を取得）
             target = analysis_blob(mask)
 
             # 面積最大ブロブの中心座標を取得
@@ -278,9 +268,6 @@ if __name__ == "__main__":
                 flag =3
                 dx.moveDXL()
 
-
-
-
             if disnum>150:
                 flag2 =1
                 dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, DXL_ID, AX_MOVING_SPEED, 0)
@@ -297,14 +284,8 @@ if __name__ == "__main__":
                 flag2 =0
 
             print('flag:',flag,'flag2:',flag2)
-
-            #print (img.shape[1]//2)
-            #print (center_x)
-            #cv2.imshow('Camera',img)
             if cv2.waitKey(1) & 0xff == 27:
                 break
-
-
 ##############################################################################
 
     finally:
